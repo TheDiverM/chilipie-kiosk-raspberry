b9a2e97318952f940533f0ccf8b6572adea7ffeb
Morten <mmoxnes@gmail.com>
2025-01-19 14:04:18 +0100

    noen ekstra filer for boot
    oppdatering av filer

---
+++ boot/firstrun.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set +e
+
+FIRSTUSER=`getent passwd 1000 | cut -d: -f1`
+FIRSTUSERHOME=`getent passwd 1000 | cut -d: -f6`
+if [ -f /usr/lib/userconf-pi/userconf ]; then
+   /usr/lib/userconf-pi/userconf 'pi' '$5$5WBjWc5mM5$J/bL43/D9PY1zR5FZgOcufWq6o.TPE6wh2oA37yS3CC'
+else
+   echo "$FIRSTUSER:"'$5$5WBjWc5mM5$J/bL43/D9PY1zR5FZgOcufWq6o.TPE6wh2oA37yS3CC' | chpasswd -e
+   if [ "$FIRSTUSER" != "pi" ]; then
+      usermod -l "pi" "$FIRSTUSER"
+      usermod -m -d "/home/pi" "pi"
+      groupmod -n "pi" "$FIRSTUSER"
+      if grep -q "^autologin-user=" /etc/lightdm/lightdm.conf ; then
+         sed /etc/lightdm/lightdm.conf -i -e "s/^autologin-user=.*/autologin-user=pi/"
+      fi
+      if [ -f /etc/systemd/system/getty@tty1.service.d/autologin.conf ]; then
+         sed /etc/systemd/system/getty@tty1.service.d/autologin.conf -i -e "s/$FIRSTUSER/pi/"
+      fi
+      if [ -f /etc/sudoers.d/010_pi-nopasswd ]; then
+         sed -i "s/^$FIRSTUSER /pi /" /etc/sudoers.d/010_pi-nopasswd
+      fi
+   fi
+fi
+if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_keymap 'no'
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_timezone 'Europe/Oslo'
+else
+   rm -f /etc/localtime
+   echo "Europe/Oslo" >/etc/timezone
+   dpkg-reconfigure -f noninteractive tzdata
+cat >/etc/default/keyboard <<'KBEOF'
+XKBMODEL="pc105"
+XKBLAYOUT="no"
+XKBVARIANT=""
+XKBOPTIONS=""
+
+KBEOF
+   dpkg-reconfigure -f noninteractive keyboard-configuration
+fi
+rm -f /boot/firstrun.sh
+sed -i 's| systemd.run.*||g' /boot/cmdline.txt
+exit 0
